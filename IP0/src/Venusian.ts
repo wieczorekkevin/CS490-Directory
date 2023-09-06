82f4eb5e011004dd2b4f240a98cd021c0118e22c
wieczorekkevin <wieczorek.kevin.m@gmail.com>
2023-09-06 13:51:12 -0400

    Finished Venusian Class

--- IP0/src/Venusian.ts
+++ IP0/src/Venusian.ts
@@ -1,2 +1,19 @@
 export default class Venusian {
+    name: string;
+    VSN: number;
+    static uniqueIdentifier: number = 1;
+    
+    constructor(name:string) {
+        this.name = name;
+        this.VSN = Venusian.uniqueIdentifier++;
+    }
+
+    //Public Methods
+    getName() {
+        return this.name;
+    }
+
+    getVsn() {
+        return this.VSN;
+    }
 } 
--- TestingFolder/hello-world.ts
+++ TestingFolder/hello-world.ts
@@ -1,1 +1,36 @@
+export{};
+
+class Venusian {
+    name: string;
+    VSN: number;
+    static currentID: number = 1;
+    
+    constructor(name:string) {
+        this.name = name;
+        this.VSN = Venusian.currentID++;
+    }
+
+    //Public Methods
+    getName() {
+        return this.name;
+    }
+
+    getVsn() {
+        return this.VSN;
+    }
+} 
+
 console.log('Hello, World!');
+
+let greeting: (name: string) => string;
+
+greeting = function (name: string) {
+    return `Hi ${name}`;
+}
+
+const testVenusian = new Venusian('Kevin');
+console.log(greeting(testVenusian.name));
+console.log('Hello ' + testVenusian.name + ', your VSN is: ' + testVenusian.VSN);
+
+console.log(testVenusian.getName() + ' ' + testVenusian.getVsn());
+
