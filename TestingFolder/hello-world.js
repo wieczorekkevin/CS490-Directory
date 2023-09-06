7de78c09f390ff5f7013313b54c3b8f361fabb95
wieczorekkevin <wieczorek.kevin.m@gmail.com>
2023-09-06 13:49:49 -0400

    Finished Venusian class

--- TestingFolder/hello-world.js
+++ TestingFolder/hello-world.js
@@ -1,1 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Venusian = /** @class */ (function () {
+    function Venusian(name) {
+        this.name = name;
+        this.VSN = Venusian.currentID++;
+    }
+    //Public Methods
+    Venusian.prototype.getName = function () {
+        return this.name;
+    };
+    Venusian.prototype.getVsn = function () {
+        return this.VSN;
+    };
+    Venusian.currentID = 1;
+    return Venusian;
+}());
 console.log('Hello, World!');
+var greeting;
+greeting = function (name) {
+    return "Hi ".concat(name);
+};
+var testVenusian = new Venusian('Kevin');
+console.log(greeting(testVenusian.name));
+console.log('Hello ' + testVenusian.name + ', your VSN is: ' + testVenusian.VSN);
+console.log(testVenusian.getName() + ' ' + testVenusian.getVsn());
